ee9f39ecd501db35caf9cc31b49d782e283adbab
Mahmoud Ramadan <mramra1980@gmail.com>
2022-12-31 21:03:27 +0200

    web

--- web.py
+++ web.py
@@ -1,21 +1,51 @@
+
 from flask import Flask, render_template
+import mariadb
 
-app = Flask(__name__)
+conn = mariadb.connect(
+        user="root",
+        password="password",
+        host="localhost",
+        port=3306,
+        database="Students")
+cur = conn.cursor()
 
+query = f"""SELECT student_id,student_name,email,mobile_number,address_name,level_name,BOD
+			 FROM students
+			 join contacts c on students.contact_id = c.contact_id
+             join addresss a on students.address_id = a.address_id
+             join levels l on l.level_id = students.level_id"""
+cur.execute(query)
+LevelStudent = cur.fetchall()
 
+query = f"""select course_id,level_name,cource_name,max_capacity,rate_per_hour,total_hours 
+			from courses
+			join levels l on courses.level_id = l.level_id"""
+cur.execute(query)
+LevelCourses = cur.fetchall()
+
+query = f"""select course_schedule_id,cource_name,day,duration,start_time,end_time
+			from course_schedules
+			join courses on courses.course_id = course_schedules.course_id"""
+cur.execute(query)
+LevelCourseSchedule = cur.fetchall()
+
+app = Flask(__name__, template_folder='template')
 @app.route("/")
 def index():
 	return render_template("index.html")
+@app.route("/courses")
+def courses():
+	return render_template("courses.html", courses=LevelCourses)
+@app.route("/CourseSchedule")
+def CourseSchedule():
+	return render_template("CourseSchedule.html", CourseSchedule=LevelCourseSchedule)
+@app.route("/student")
+def student():
+	return render_template("student.html", student=LevelStudent)
 @app.route("/<name>")
 def welcome(name):
 	return render_template("welcome.html", name=name)
-@app.route("/home")
-def home():
-	return render_template("home.html")
-@app.route("/about")
-def about():
-	sites = ['twitter', 'facebook', 'instagram', 'whatsapp']
-	return render_template("about.html", sites=sites)
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
